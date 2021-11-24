47242e74d2bc2b09dee7bdbe5a5eefaee173a2ec
kperkins97 <perkins.kelsey@yahoo.com>
2021-11-24 13:04:01 -0600

    added an s on belongsTo

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongTo(Category, {
+Product.belongsTo(Category, {
 foreignKey: 'category_id'
 })
 // Categories have many Products
@@ -14,7 +14,7 @@ Category.hasMany(Product, {
 
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     foreignKey: 'product_id',
@@ -22,7 +22,7 @@ Product.belongToMany(Tag, {
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
     through: {
       model: ProductTag,
       foreignKey: 'tag_id',
